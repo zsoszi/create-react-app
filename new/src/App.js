fe2026d2b9ede848e5c1df4587404dee5288c38e
zsoszi <zsolt.szilagyi86@gmail.com>
2021-01-19 13:04:20 +0100

    new file
    add button, and change color event

--- new/src/App.js
+++ new/src/App.js
@@ -1,14 +1,25 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function App(){
+  
+const [color, setColor] = useState()
+const randomColor = () => {
+  const random = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  setColor(random);
+};
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to React</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +28,7 @@ function App() {
         >
           Learn React
         </a>
+        <button style={{background: `${color}`}} onClick={randomColor}>Click me!</button>
       </header>
     </div>
   );
